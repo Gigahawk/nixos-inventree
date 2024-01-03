20e0d1ded3ec6e94991a114c7b50bc4cb9eae72e
Jasper Chan <jasperchan515@gmail.com>
2024-01-02 23:35:59 -0800

    inventree-server: allow passing extra args to gunicorn

--- flake.nix
+++ flake.nix
@@ -85,7 +85,7 @@
           text = ''
             INVENTREE_SRC=${self.packages.${system}.inventree-src}/src
             pushd $INVENTREE_SRC/InvenTree
-            gunicorn -c gunicorn.conf.py InvenTree.wsgi
+            gunicorn -c gunicorn.conf.py InvenTree.wsgi "$@"
             popd
           '';
         };
